ae451ca392520e4a71f4c1ddff3ff680bfcd88e0
TrNgTien <ngoctien27122001@gmail.com>
2021-09-11 17:16:23 +0700

    change display

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Welcome to the app snippet"));
+app.listen(PORT, () =>
+  console.log(`Welcome to the app snippet with port : ${PORT} `)
+);
 
 //Routers
 app.use("/auth", userRoutes);
